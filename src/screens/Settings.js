7beb18eecfe30b8eb4938381ffbb51975879bd5c
seunope <seunope2004@yahoo.co.uk>
2021-02-23 14:18:47 +0100

    navigating between screens

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -4,5 +4,9 @@ import {Button, SafeAreaView} from 'react-native';
 export default ({navigation}) => (
   <SafeAreaView>
     <Button title="Toggle Drawer" onPress={() => navigation.toggleDrawer()} />
+    <Button
+      title="To Action"
+      onPress={() => navigation.navigate('Tabs', {screen: 'Actions'})}
+    />
   </SafeAreaView>
 );
